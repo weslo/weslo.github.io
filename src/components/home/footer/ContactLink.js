0eb6a06305a9b5d82b762bbb48ac3b30371be62d
Weslo <wesley.rockholz@gmail.com>
2017-10-07 23:05:10 -0400

    Fix bug with opening resume in new tab'

--- src/components/home/footer/ContactLink.js
+++ src/components/home/footer/ContactLink.js
@@ -7,7 +7,7 @@ class ContactLink extends Component {
         return m('li.contact-link', [
             m('a', {
                 href: vnode.attrs.url,
-                target: vnode.attrs.target === 'undefined' ? "_blank" : vnode.attrs.target
+                target: "target" in vnode.attrs ? vnode.attrs.target : "_blank"
             }, [
                 m('i', { class: "fa fa-fw fa-2x " + vnode.attrs.icon }),
                 m('h3', vnode.attrs.text)
